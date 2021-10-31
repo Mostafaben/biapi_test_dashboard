bb324565cafec63b22d99bbf6d62524b7e4b25a5
mostapha.b <mostapha.b@inside360.dz>
2021-10-31 11:36:15 +0100

    [ADD] binding data from the template

--- src/app/modules/users-managements/components/users-listings/users-listings.component.ts
+++ src/app/modules/users-managements/components/users-listings/users-listings.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-users-listings',
-  templateUrl: './users-listings.component.html',
-  styleUrls: ['./users-listings.component.scss']
+  selector: "app-users-listings",
+  templateUrl: "./users-listings.component.html",
+  styleUrls: ["./users-listings.component.scss"]
 })
 export class UsersListingsComponent implements OnInit {
+  /**
+   * @Note
+   * here selected view will change the view from list view to grid view
+   * LIST VIEW: false
+   * GRID VIEW: true
+   * @default GRID_VIEW
+   */
+  public isGridView: boolean = true;
+  public searchInputValue: string = "";
+  public LIST_ITEM_SIZES: number[] = [25, 50, 100];
+  public selectedListPageSize: number = 0;
+  public selectedOrderListBy: number = 0;
+  public currentPage: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleChangeView() {
+    this.isGridView = !this.isGridView;
   }
 
+  handleSearchInputChange() {
+    console.log(this.searchInputValue);
+  }
+  handleOrderByChange() {
+    console.log(this.selectedOrderListBy);
+  }
+
+  /**
+   *
+   * @param direction {NUMBER} if positive than next page
+   * else previous page
+   *
+   */
+  changePage(direction: number) {}
 }
--- src/app/modules/users-managements/users-managements.module.ts
+++ src/app/modules/users-managements/users-managements.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from "@angular/common";
 import { UsersManagementsRoutingModule } from "./users-managements-routing.module";
 import { UsersListingsComponent } from "./components/users-listings/users-listings.component";
 import { SharedModule } from "src/app/shared/modules/shared.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [UsersListingsComponent],
-  imports: [CommonModule, UsersManagementsRoutingModule, SharedModule]
+  imports: [
+    CommonModule,
+    UsersManagementsRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule
+  ]
 })
 export class UsersManagementsModule {}
